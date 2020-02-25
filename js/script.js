f91a7fbf1580ff68d4dca0439ed076a043b8dbba
tranel-pdx <59620262+tranel-pdx@users.noreply.github.com>
2020-02-24 19:52:04 -0800

    0224

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
 // your script file for adding your own jquery
 $(function() {
 // Your Code from here on down. Don't delete that line above!
-  
+const init = function(){
+let items = document.querySelectorAll('section');
+for (let i = 0; i < items.length; i++){
+  items[i].style.background = randomColor({luminosity: 'light'});
+}
+cssScrollSnapPolyfill()
+}
+init();
 
 
 
